3914b68675f24b0abe92709c0bc8ecaa985a806b
Yufeng Guo <guo_yufeng@outlook.com>
2018-06-08 00:17:04 +0800

    initial commit

---
+++ algcoding/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using algcoding.chap1;
+
+namespace algcoding
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var stack = new StackGetMin();
+            stack.push(5);
+            stack.push(3);
+            stack.push(3);
+            Console.WriteLine(stack.GetMin());
+            stack.push(1);
+            stack.GetMin();
+            stack.pop();
+
+        }
+    }
+}
---
+++ algcoding/chap1/StackGetMin.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+
+namespace algcoding.chap1
+{
+    public class StackGetMin
+    {
+        private Stack stackData;
+        private Stack stackMin;
+
+        public StackGetMin()
+        {
+            stackData = new Stack();
+            stackMin = new Stack();
+        }
+
+        public void push(int newNum)
+        {
+            stackData.Push(newNum);
+            if (stackMin.Count==0)
+            {
+                stackMin.Push(newNum);
+            }
+            else if (newNum <= this.GetMin())
+            {
+                stackMin.Push(newNum);
+            }
+        }
+
+        public int pop()
+        {
+            if(stackData.Count == 0)
+            {
+                throw new InvalidOperationException("Your stack has no number!");
+            }
+            int num = Convert.ToInt32(stackData.Pop());
+            if(num == this.GetMin())
+            {
+                stackMin.Pop();
+            }
+            return num;
+        }
+
+        public int GetMin()
+        {
+            if (stackMin.Count == 0)
+            {
+                throw new InvalidOperationException("Your stack has no number!");
+            }
+            return Convert.ToInt32(this.stackMin.Peek());
+        }
+    }
+}
